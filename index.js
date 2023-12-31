59c5fea78a8187fa8cdf7202c6024cf88612f340
dishanth <shettysamarth738@gmail.com>
2023-12-31 22:02:28 +0530

    added

--- index.js
+++ index.js
@@ -22,5 +22,5 @@ app.get('/',(req,res)=>{
 const PORT =process.env.PORT || 3000
 
 app.listen(PORT,()=>{
-    console.log(`Server  is running on ${DEV_MODE} port no ${PORT}`)
+    console.log(`Server  Is running on ${DEV_MODE} port no ${PORT}`)
 })
